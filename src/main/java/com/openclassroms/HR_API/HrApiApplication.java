5604960c99ee9010cb7eb89163d92b5fefb58392
SALIFOU Ouzéïrou <salifououzerou25@gmail.com>
2025-12-14 15:28:06 +0100

    initial commit

---
+++ src/main/java/com/openclassroms/HR_API/HrApiApplication.java
@@ -0,0 +1,13 @@
+package com.openclassroms.HR_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassroms/HR_API/HrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassroms.HR_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
