3fcaedf638042564f0e2fe6ab8d037a759a1e9f5
ouzayr-droid <salifououzerou25@gmail.com>
2025-12-25 23:59:20 +0100

    fix bug

---
+++ src/test/java/com/openclassroms/HR_API/EmployeeControllerTest.java
@@ -0,0 +1,31 @@
+package com.openclassrooms.HR_API;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.openclassroms.HR_API.controller.EmployeeController;
+import com.openclassroms.HR_API.service.EmployeeService;
+
+@WebMvcTest(controllers = EmployeeController.class)
+public class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Test
+    public void testGetEmployees() throws Exception {
+        mockMvc.perform(get("/employees"))
+            .andExpect(status().isOk());
+    }
+
+}
